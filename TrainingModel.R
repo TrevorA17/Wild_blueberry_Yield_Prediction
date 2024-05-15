c92fa576afce1c941d167f4e9f19eeee55c2715b
Trevor Augustine <122263629+TrevorA17@users.noreply.github.com>
2024-05-15 10:24:50 +0300

    Data splitting

--- TrainingModel.R
+++ TrainingModel.R
@@ -27,3 +27,24 @@ head(wild_blueberry_data)
 
 # View the dataset in a separate viewer window
 View(wild_blueberry_data)
+
+# Load the caret library for data splitting
+library(caret)
+
+# Set seed for reproducibility
+set.seed(123)
+
+# Define the proportion of data to be used for training (e.g., 70% for training, 30% for testing)
+train_proportion <- 0.7
+
+# Split the dataset into training and testing sets
+index <- createDataPartition(y = wild_blueberry_data$yield, p = train_proportion, list = FALSE)
+train_data <- wild_blueberry_data[index, ]
+test_data <- wild_blueberry_data[-index, ]
+
+# Print the dimensions of the training and testing sets
+print("Dimensions of Training Data:")
+print(dim(train_data))
+print("Dimensions of Testing Data:")
+print(dim(test_data))
+
